adb803791bb29ac2f46123d9bea6148ca7821b74
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-10 14:08:27 +0800

    Separate parser into own file

---
+++ src/implementations/dbs.rs
@@ -0,0 +1,3 @@
+pub fn parse(text: String) -> String {
+    text
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use itertools::Itertools;
 use wasm_bindgen::prelude::*;
+use implementations::dbs;
 
 mod utils;
+mod implementations;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,5 +19,5 @@ extern "C" {
 #[wasm_bindgen]
 pub fn parse(array: JsValue) -> String {
     let elements: Vec<String> = array.into_serde().unwrap();
-    elements.join("\n\n")
+    elements.into_iter().map(dbs::parse).intersperse("\n\n".into()).collect()
 }
